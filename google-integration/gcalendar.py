cb7ea93e35700632f5cbd2ad5bfbd453619b0df2
Hassan-essoufi <hassanessoufi2004@gmail.com>
2026-04-30 00:53:27 +0100

    finishing calendar service and event creation

---
+++ google-integration/gcalendar.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+from auth import get_credentials
+
+def get_calendar_service():
+    
+    try:
+        creds = get_credentials()
+        service = build('calendar', version="v3", credentials=creds)
+
+        return service
+    
+    except Exception as e:
+        raise Exception(f'[ERROR]; Credentials are invalid,{e}') 
+    
+
+def create_event(summary, start, end, description):
+
+    try:
+        service = get_calendar_service()
+        g_calendar = service.events()
+
+        event = {
+            "summary" : summary,
+            "description": description,
+            "start": start,
+            "end": end
+        }
+        result = g_calendar.insert(
+            calendarId='primary',
+            body=event
+        ).execute()
+
+        return result
+    except Exception as e:
+        raise Exception(f'[ERROR]:{e}')
+
