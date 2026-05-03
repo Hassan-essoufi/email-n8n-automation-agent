066454ce8d6d2d21ddbb60b74cac917265f5c876
Hassan-essoufi <hassanessoufi2004@gmail.com>
2026-05-03 23:40:02 +0100

    dockerfile and docker_compose final config

--- openclaw_agent/app/memory.py
+++
@@ -1,9 +0,0 @@
-from diskcache import Cache
-
-cache = Cache("./cache/processed_emails")
-
-def is_processed(email_id):
-    return True if email_id in cache else False
-
-def mark_processed(email_id):
-        cache[email_id] = True
